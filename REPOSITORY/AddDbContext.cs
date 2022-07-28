f0b2cca1302474f77ec4cdadc20193e7ad617f29
Bilge Kağan KOÇ <bilgekagan.koc@kaleyazilim.com.tr>
2022-07-28 14:58:55 +0300

    v2

---
+++ REPOSITORY/AddDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CORE;
+using Microsoft.EntityFrameworkCore;
+using REPOSITORY.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPOSITORY
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // toplu eklemek için
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration()); // tek tek eklemek için
+            base.OnModelCreating(modelBuilder); 
+        }
+
+    }
+}
---
+++ REPOSITORY/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CORE;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPOSITORY.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn(1, 2);
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
